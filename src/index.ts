4f4db2e920351db14b37dfb6f8aba7797f18e860
xuxihai <xuxh@tuwan.com>
2024-08-29 12:32:52 +0800

    update expose

--- src/index.ts
+++ src/index.ts
@@ -7,38 +7,41 @@ const env = process.env.NODE_ENV;
 
 // 获取当前 Git 提交信息
 async function getGitCommitInfo() {
-    try {
-        const commitHash = execSync("git rev-parse HEAD").toString().trim();
-        return { commitHash };
-    } catch (error) {
-        return { commitHash: "get failed" };
-    }
+  try {
+    const commitHash = execSync("git rev-parse HEAD").toString().trim();
+    return { commitHash };
+  } catch (error) {
+    return { commitHash: "get failed" };
+  }
 }
 
 function getPackageJson() {
-    // 获取 package.json 信息
-    return JSON.parse(readFileSync(path.resolve(cwd, "package.json"), "utf-8"));
+  // 获取 package.json 信息
+  return JSON.parse(readFileSync(path.resolve(cwd, "package.json"), "utf-8"));
 }
 
 // Vite 插件
-export function injectInfoPlugin() {
-    return {
-        name: "vite-plugin-inject-info",
-        async transformIndexHtml(html:string) {
-            if (env !== "production") {
-                return html;
-            }
-            const packageJson = getPackageJson();
-            const gitInfo = await getGitCommitInfo();
-            const info = {
-                name: packageJson.name,
-                version: packageJson.version,
-                mode: env,
-                commit: gitInfo.commitHash,
-                buildtime: new Date().toLocaleString(),
-            };
-            const injectCode = `console.log('build info:',${JSON.stringify(info)});`;
-            return html.replace("</head>", `<script>${injectCode}</script></head>`);
-        },
-    };
+export function viteBuildInfo() {
+  return {
+    name: "vite-plugin-inject-info",
+    async transformIndexHtml(html: string) {
+      if (env !== "production") {
+        return html;
+      }
+      const packageJson = getPackageJson();
+      const gitInfo = await getGitCommitInfo();
+      const info = {
+        name: packageJson.name,
+        version: packageJson.version,
+        mode: env,
+        commit: gitInfo.commitHash,
+        buildtime: new Date().toLocaleString(),
+      };
+      const injectCode = `console.log('build info:',${JSON.stringify(info)});`;
+      return html.replace("</head>", `<script>${injectCode}</script></head>`);
+    },
+  };
 }
+
+// 0.0.1 used
+export let injectInfoPlugin = viteBuildInfo;
