89ccf83f77039d385157dc4c5aa983c2e66150ff
xuxihai <xuxihai123@gmail.com>
2024-09-20 09:42:16 +0800

    support inject to body or head opt

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,12 @@ import { execSync } from "child_process";
 const cwd = process.cwd();
 const env = process.env.NODE_ENV;
 
+interface BuildInfoOpts {
+  type?: string;
+}
+
 // 获取当前 Git 提交信息
-async function getGitCommitInfo() {
+function getGitCommitInfo() {
   try {
     const commitHash = execSync("git rev-parse HEAD").toString().trim();
     return { commitHash };
@@ -21,15 +25,15 @@ function getPackageJson() {
 }
 
 // Vite 插件
-export function viteBuildInfo() {
+export function viteBuildInfo(opts: BuildInfoOpts = {}) {
   return {
     name: "vite-plugin-inject-info",
-    async transformIndexHtml(html: string) {
+    transformIndexHtml(html: string) {
       if (env !== "production") {
         return html;
       }
       const packageJson = getPackageJson();
-      const gitInfo = await getGitCommitInfo();
+      const gitInfo = getGitCommitInfo();
       const info = {
         name: packageJson.name,
         version: packageJson.version,
@@ -37,8 +41,18 @@ export function viteBuildInfo() {
         commit: gitInfo.commitHash,
         buildtime: new Date().toLocaleString(),
       };
-      const injectCode = `console.log('build info:',${JSON.stringify(info)});`;
-      return html.replace("</head>", `<script>${injectCode}</script></head>`);
+      const code = `window.__buildinfo__=${JSON.stringify(info)}`;
+      const injectCode = `${code};console.log(window.__buildinfo__);`;
+      if (opts.type === "head") {
+        return html.replace(
+          "</head>",
+          `<script>${injectCode}</script>
+          </head>`
+        );
+      } else {
+        return html.replace("<body>", `<body>
+          <script>${injectCode}</script>`);
+      }
     },
   };
 }
